c0d90cfb6a618288d178ea523bb45c1d3b059dd6
seiichi-hirose <hirose@glocalcode.com>
2023-09-28 07:08:12 +0900

    コンタクトページをcontactform7で実装

--- WordPressTheme/page-contact.php
+++ WordPressTheme/page-contact.php
@@ -21,73 +21,7 @@
     <div class="p-lower-contact__inner l-inner">
     <p class="p-lower-contact__header">Webサイトの制作のご依頼やお見積りなど、<br class="u-mobile">お気軽にご相談ください。</p>
 
-    <form class="p-lower-contact__form c-contact-form">
-        <ul>
-
-            <!-- アイテム お問い合わせ種別 -->
-            <li class="c-contact-form__item">
-                <dl class="c-contact-form__flex">
-                    <dt class="c-contact-form__title c-contact-form__title--radio">お問い合わせ種別<span class="c-contact-form__danger-label">*必須</span></dt>
-                    <dd class="c-contact-form__radios">
-                        <label for="consult"><input class="c-contact-form__radio" type="radio" name="kinds" id="consult" value="お仕事のご依頼・ご相談">お仕事のご依頼・ご相談</label>
-                        <label for="estimate"><input class="c-contact-form__radio" type="radio" name="kinds" id="estimate" value="お見積りのご依頼">お見積りのご依頼</label>
-                        <label for="others"><input class="c-contact-form__radio" type="radio" name="kinds" id="others" value="その他">その他</label>
-                    </dd>
-                </dl>
-            </li>
-
-            <!-- アイテム 氏名 -->
-            <li class="c-contact-form__item">
-                <dl class="c-contact-form__flex">
-                    <dt class="c-contact-form__title"><label for="namae">お名前</label><span class="c-contact-form__danger-label">*必須</span></dt>
-                    <dd>
-                        <input class="c-contact-form__input" type="text" id="namae">
-                    </dd>
-                </dl>
-            </li>
-            <!-- アイテム 会社名 -->
-            <li class="c-contact-form__item">
-                <dl class="c-contact-form__flex">
-                    <dt class="c-contact-form__title"><label for="firm">会社名</label></dt>
-                    <dd>
-                        <input class="c-contact-form__input" type="text" id="firm">
-                    </dd>
-                </dl>
-            </li>
-            <!-- アイテム メールアドレス -->
-            <li class="c-contact-form__item">
-                <dl class="c-contact-form__flex">
-                    <dt class="c-contact-form__title"><label for="mail">メールアドレス</label><span class="c-contact-form__danger-label">*必須</span></dt>
-                    <dd>
-                        <input class="c-contact-form__input" type="text" id="mail">
-                    </dd>
-                </dl>
-            </li>
-            <!-- アイテム 電話番号 -->
-            <li class="c-contact-form__item">
-                <dl class="c-contact-form__flex">
-                    <dt class="c-contact-form__title"><label for="tel">お電話番号</label><span class="c-contact-form__limit-text">（半角数字ハイフンなし）</span></dt>
-                    <dd>
-                        <input class="c-contact-form__input" type="text" id="tel">
-                    </dd>
-                </dl>
-            </li>
-
-            <!-- アイテム お問い合わせ内容 -->
-            <li class="c-contact-form__item">
-                <dl class="c-contact-form__flex">
-                    <dt class="c-contact-form__title"><label for="textarea">お問い合わせ内容</label><span class="c-contact-form__danger-label">*必須</span></dt>
-                    <dd><textarea class="c-contact-form__textarea" id="textarea"></textarea></dd>
-                </dl>
-            </li>
-
-        </ul>
-
-        <!-- ボタン -->
-        <div class="c-contact-form__btn p-works__btn">
-            <a href="#" class="c-button">送信する</a>
-        </div>
-    </form>
+    <?php echo do_shortcode('[contact-form-7 id="c8e5bdf" title="お問い合わせ"]'); ?>
 
     </div>
 </div>
