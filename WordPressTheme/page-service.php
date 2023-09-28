914b684de569750d4da5beea3e43f03d23b400fd
seiichi-hirose <hirose@glocalcode.com>
2023-09-28 10:35:54 +0900

    page-service.php作成

---
+++ WordPressTheme/page-service.php
@@ -0,0 +1,97 @@
+<?php get_header(); ?>
+
+<section class="l-lower-mv p-lower-mv  js-mv-height">
+    <div class="p-lower-mv__inner">
+        <div class="p-lower-mv__header">
+            <h1 class="p-lower-mv__title">SERVICE</h1>
+            <p class="p-lower-mv__subtitle">事業内容</p>
+        </div>
+    </div>
+</section>
+
+<div class="c-breadcrumb">
+    <div class="c-breadcrumb__inner l-inner">
+        <a href="#" class="c-breadcrumb__link-top">TOP</a>
+        <span class="c-breadcrumb__arrow">></span>
+        <a href="" class="c-breadcrumb__link">SERVICE</a>
+    </div>
+</div>
+
+<div class="p-lower-service l-lower-frame">
+    <div class="p-lower-service__inner l-inner">
+
+        <div class="p-lower-service__wrapper">
+            <div class="p-lower-service__card">
+                <div class="p-lower-service__card-img">
+                    <img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/web-produce.jpg" alt="サービス画像1">
+                </div>
+                <div class="p-lower-service__card-body">
+                    <h3 class="p-lower-service__card-title">Webサイト制作</h3>
+                    <p class="p-lower-service__card-text">
+                    新規サイトはもちろん、サイトリニューアルやランディングページ制作も承っております。<br>
+                    サイトのゴールはお客様の夢や目的を実現することです。そのためにまずはしっかりとお話をうかがい、サイトに必要な要素を洗い出します。その後、ワイヤーフレーム（サイトのレイアウト）の作成、デザインの制作、コーディングと進みます。制作の過程でお客様とのお打ち合わせを数回実施させていただき、ご要望とご意見を反映しながらサイトを制作していきます。
+                    </p>
+                </div>
+            </div>
+            <div class="p-lower-service__card">
+                <div class="p-lower-service__card-img">
+                    <img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/coding.jpg" alt="サービス画像2">
+                </div>
+                <div class="p-lower-service__card-body">
+                    <h3 class="p-lower-service__card-title">コーディング</h3>
+                    <p class="p-lower-service__card-text">
+                        CSS設計（予測・再利用・保守・拡張できる）に基づき作業いたします。<br>
+                        <br>
+                        以下、具体的な対応スキルです。<br>
+                        ・HTML/CSS: デザインの意図を汲み取りつつ、正確な作業を心がけています。<br>
+                        ・JavaScript: 単に動きを与える以上に、命を宿す気持ちで取り組んでいます。<br>
+                        ・WordPress: オリジナルテーマにより、扱いやすさを重視し構築しています。<br>
+                        ・Git: 保守管理のため、チーム開発はもとより個人開発でも利用しています。
+                    </p>
+                </div>
+            </div>
+            <div class="p-lower-service__card">
+                <div class="p-lower-service__card-img">
+                    <img src="<?php echo get_theme_file_uri(); ?>/assets/images/common/design.jpg" alt="サービス画像3">
+                </div>
+                <div class="p-lower-service__card-body">
+                    <h3 class="p-lower-service__card-title">Webデザイン</h3>
+                    <p class="p-lower-service__card-text">
+                    デザイン制作で重視するポイントは以下の通りです。<br>
+                    １、常にトレンドを追いかけ、敏感でいること<br>
+                    ２、実装一つ一つに明確な意図を持たせること<br>
+                    ３、直感的な使いやすさを追求すること<br>
+                    ４、アクセシビリティの向上に努めていくこと<br>
+                    <br>
+                    以上を、PhotoShop・Figma・Canvaなどのツールで表現していきます。
+                    </p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+
+
+
+
+<section class="l-contact p-contact">
+    <div class="p-contact__inner l-inner">
+      <div class="p-contact__title c-section-header">
+        <h2 class="c-section-header__engtitle">CONTACT</h2>
+        <p class="c-section-header__jatitle">お問い合わせ</p>
+      </div>
+      <div class="p-contact__contents">
+        <div class="p-contact__text-block">
+          <p class="p-contact__text">Webサイトの制作のご依頼やお見積りなど、<br>お気軽にご相談ください。</p>
+        </div>
+        <div class="p-contact__btn">
+          <a href="<?php echo $contact ?>" class="c-button">more</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<?php get_footer(); ?>
